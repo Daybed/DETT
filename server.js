0a57935cdfbbcacb51073911bd2ec9d3670252b3
David Chaillé <david.chaille@carbip.com>
2016-10-05 15:38:07 +0200

    changing the response in html

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ http.createServer(function(request, response){
 	response.writeHead(200, {
 		'Content-type' : 'text/plain'
 	});
-	response.write('Natasha est ready ! :)')
+	response.write('Le serveur est lancé et fonctionne sur le port 1337')
 	response.end();
 	console.log('Le serveur fonctionne sur le port 1337');
 }).listen(1337);
